15ee118f0fddbb80c03707bbb9f7c492403385a8
Tyler Tian <tylertian123@gmail.com>
2019-04-25 21:36:28 -0400

    Add flap stuff

--- src/main/java/com/arctos6135/myoflap/Main.java
+++ src/main/java/com/arctos6135/myoflap/Main.java
@@ -1,7 +1,10 @@
 package com.arctos6135.myoflap;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
 import com.arctos6135.myoflap.myo.DataCollector;
-import com.arctos6135.myoflap.myo.EulerOrientation;
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
 import com.thalmic.myo.MyoException;
@@ -11,6 +14,8 @@ public class Main {
     public static Hub hub;
     public static Myo myo;
     public static DataCollector collector = new DataCollector();
+
+    public static Robot robot;
 
     public static void main(String[] args) {
 
@@ -51,21 +56,64 @@ public class Main {
             System.exit(1);
         }
 
+        while(collector.getOrientationQuat() == null) {
+            hub.runOnce(1000);
+        }
+        collector.setRefOrientation(collector.getOrientationQuat());
+
+        long timeFlapLow = 0;
+        long timeFlapHigh = 0;
+        int counter = 0;
+        
+        try {
+            robot = new Robot();
+        }
+        catch(AWTException e) {
+            System.err.println("ERROR: Cannot create Robot");
+            System.exit(3);
+        }
+
+        double mid = -13;
+        double highest = Double.NEGATIVE_INFINITY;
+        double lowest = Double.POSITIVE_INFINITY;
+
         while (true) {
-            // // Run the hub until one event occurs
-            // hub.runOnce(1000);
-            // Run the hub for 100ms
-            // Used during testing to not update the display too fast
-            hub.run(100);
+            // Run the hub until one event occurs
+            hub.runOnce(1000);
             // Only do stuff if the Myo is actually on the arm
             if (collector.onArm()) {
-                EulerOrientation orientation = collector.getOrientationEuler();
+                double pitch = collector.getOrientationEuler().getPitchDegrees();
+                
+                //System.out.println("Pitch: " + pitch);
+                if(pitch > highest) {
+                    highest = pitch;
+                }
+                if(pitch < lowest) {
+                    lowest = pitch;
+                }
+                long time = System.currentTimeMillis();
+                if(pitch > mid + 1) {
+                    if(time - timeFlapHigh <= 3000) {
+                        System.out.println("\u001b[4m\u001b[1mFlap!\u001b[0m");
+                        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                        //mid = (lowest + highest) / 2;
+                        highest = Double.NEGATIVE_INFINITY;
+                        lowest = Double.POSITIVE_INFINITY;
+                    }
+                    timeFlapHigh = 0;
+                    timeFlapLow = time;
+                }
+                else if(pitch < mid - 1) {
+                    if(timeFlapHigh == 0 && time - timeFlapLow <= 3000) {
+                        timeFlapHigh = time;
+                    }
+                }
 
-                System.out.printf("Yaw=%.3f\tPitch=%.3f\tRoll=%.3f\n", orientation.getYawDegrees(),
-                        orientation.getPitchDegrees(), orientation.getRollDegrees());
-            }
-            else {
-                System.out.println("Myo is not on an arm right now.");
+                if(counter++ > 10) {
+                    System.out.printf("Pitch: %.2f\tBounds: %.2f\t%.2f\nTimes: %d\t%d\n", pitch, mid + 1, mid - 1,
+                            timeFlapLow, timeFlapHigh);
+                }
             }
         }
     }
